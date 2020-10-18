9d271ebc5aa04099d3c3ec11174bb1b252329aaa
danij <scripty@outlook.de>
2020-10-18 14:02:53 +0200

    add example package

--- examples/client/Example.jsx
+++ examples/client/Example.jsx
@@ -1,17 +1,36 @@
 import React from 'react';
-import { Description } from './Description';
+import { Description, Code, Component, Properties } from '@scripty/react-examples';
 import { ReactSkeleton } from '../../src';
 
 export const Example = () => {
 
+    const code = `import { ReactSkeleton } from '@scripty/react-skeleton';
+
+const App = () => {
+    return <ReactSkeleton color={'#000'}/>
+}
+`
+
+    const propertiesData = [
+        {
+            property: 'color',
+            description: 'hex color',
+            type: 'string',
+            default: '#fff'
+        }
+    ]
+
     return (
-        <Description title={'Simple Card'}>
-            <ReactSkeleton
-                title={'Simple Card'}
-                width={300}
-            >
-                Simple Card Content
-            </ReactSkeleton>
+        <Description title={'Simple Description Component'}>
+            <Component>
+                <ReactSkeleton color={'#000'}/>
+            </Component>
+
+            <Code>
+                {code}
+            </Code>
+
+            <Properties data={propertiesData}/>
         </Description>
     );
 };
--- src/Styles.jsx
+++ src/Styles.jsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-    background-color: black;
+    background-color: #ccc;
+    padding: 5px;
     color: ${props => props.color };
 `;
